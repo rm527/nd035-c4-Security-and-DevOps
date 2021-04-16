6d53f6cf162487553a94c496caa1ac394294c167
geekmark1995 <geekmark1995@163.com>
2021-04-16 01:13:00 -0700

    "Adding Authentication and Authorization"

---
+++ starter_code/src/main/java/com/example/demo/security/WebSecurityConfiguration.java
@@ -0,0 +1,37 @@
+package com.example.demo.security;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    public static final String SIGN_UP_URL = "/api/user/create";
+
+    private UserDetailsServiceImpl userDetailsService;
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public WebSecurityConfiguration(UserDetailsServiceImpl userDetailsService, BCryptPasswordEncoder passwordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+                .addFilter(new JWTAuthorizationFilter(authenticationManager()))
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authenticationManager) throws Exception {
+        authenticationManager.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+    }
+}
